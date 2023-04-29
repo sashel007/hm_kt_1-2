bce4d0af4b29db06537772aa433663ba296d9eed
Alexander Sidorkin <sashel007@yandex.ru>
2023-04-29 20:41:25 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var amount = 0.0
+    val maxLimitPerDayForCard = 150_000.0
+    val maxLimitPerMonthForCard = 600_000.0
+    val maxLimitByTimeVK = 15_000.0
+    val maxLimitPerMonthVK = 40_000.0;
+
+
+
+}
