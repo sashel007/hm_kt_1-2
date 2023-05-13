caf6610a2980e9512dfb831d4f7bd2f58217e298
Alexander Sidorkin <sashel007@yandex.ru>
2023-05-13 15:06:58 +0300

    errors solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 import java.util.*
 
-@Suppress("NAME_SHADOWING")
 fun main() {
 
     val commissionInPercentage = 0.0075
     var commission = 0.0
+    val minCommission = 35.0
 
     println("Какой картой вы пользуетесь? Введите номер:")
     println("1. Maestro")
@@ -13,68 +13,28 @@ fun main() {
     println("4. МИР")
     println("5. VK Pay")
 
-    val scanner = Scanner(System.`in`)
-
-    var selectedCard = scanner.nextInt()
+    var selectedCard = readln().toInt()
     if ((selectedCard < 1) && (selectedCard > 5)) {
         print("Введите номер корректно: ")
-        selectedCard = scanner.nextInt()
+        selectedCard = readln().toInt()
     }
 
-
     println("Введите сумму: ")
-    val inputAmount = scanner.nextDouble()
+    val inputAmount = readln().toDouble()
     if (inputAmount <= 0.0) {
         print("Введите корректную сумму:")
-        scanner.nextDouble()
+        inputAmount
     }
 
     when (selectedCard) {
-        1 -> {
-            if ((inputAmount > 300.0) && (inputAmount < 75000.0)) {
-                    print("Комиссия составляет: $commission")
-            } else {
-                val commission = inputAmount * commissionInPercentage
-                if (commission < 35.0) {
-                    val commission = 35.0
-                    print("Комиссия составляет: ${(commission).toInt()}")
-                }
-                print("Комиссия составляет: ${(commission).toInt()}")
-            }
-        }
-
-        2 -> {
-            if ((inputAmount > 300.0) && (inputAmount < 75000.0)) {
-                print("Комиссия составляет: ${(commission).toInt()}")
-            } else {
-                val commission = inputAmount * commissionInPercentage
-                if (commission < 35.0) {
-                    val commission = 35.0
-                    print("Комиссия составляет: ${(commission).toInt()}")
-                }
-                print("Комиссия составляет: ${(commission).toInt()}")
-            }
-        }
-
-        3 -> {
+        in 1 .. 5 -> {
             commission = inputAmount * commissionInPercentage
             if (commission < 35.0) {
-                print("Ваша комиссия составляет: ${(commission).toInt()}")
+                print("Ваша комиссия составляет: ${minCommission.toInt()}")
+            } else {
+                print("Комиссия сос тавляет: ${(commission).toInt()}")
             }
-            print("Комиссия составляет: ${(commission).toInt()}")
         }
 
-        4 -> {
-            commission = inputAmount * commissionInPercentage
-            if (commission < 35.0) {
-                print("Ваша комиссия составляет: ${(commission).toInt()}")
-            }
-            print("Комиссия составляет: ${(commission).toInt()}")
-        }
-
-        5 -> {
-            commission = inputAmount * commissionInPercentage
-            print("Ваша комиссия составляет: ${(commission).toInt()}")
-        }
     }
 }
